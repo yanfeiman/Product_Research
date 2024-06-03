bf7a16dd4db877ea665bf84a4de4994b07fdae98
Amy Weng <90800337+amycweng@users.noreply.github.com>
2024-06-03 11:02:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import plotly.express as px
 from collections import Counter
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
+
 #######################
 st.set_page_config(
     page_title="Amazon Jewelry Dashboard",
@@ -46,6 +48,7 @@ st.markdown("""
 products = pd.read_csv('data/products.csv')
 products = products.drop(columns=['Unnamed: 0'])
 stats = pd.read_csv('data/stats.csv')
+stats['Product Type'] = stats['Product Type'].astype(str)
 
 allterms = {}
 for keyword in products.Keyword:
@@ -60,51 +63,57 @@ with st.sidebar:
     categories = ['All']
     categories.extend([term.capitalize() for term in allterms])
     selected_category = st.selectbox('Select a keyword for a category of products', categories)
-    metrics = {}
-
+    
     if selected_category == 'All': 
         df = products
-        for term in stats.Title: 
+        metrics = {}
+        for idx, term in enumerate(stats.Title):
+            p_type = stats['Product Type'][idx]
+            term = f"{p_type} - {term}" 
             if term not in metrics: metrics[term] = None 
-        metrics['Frequent Phrases'] = None 
+            if f"{p_type} - Frequent Phrases" not in metrics: metrics[f"{p_type} - Frequent Phrases"] = None 
     else: 
         df = products[products.Keyword == selected_category.lower()]
-        # select_searchterm = st.selectbox('Select a search term associated with the keyword above', searchterms)
-        # df = df[df.Search_Term == select_searchterm]
-        # metrics = ["Frequent Phrases","Minimum_Revenue","Price","Rating","Reviews_Count","Best_Seller","Sales_Volume_Past_Month","Prime","Price Strikethrough"]
-        # metrics = ["Frequent Phrases","Minimum Revenue","Prices","Feedback","Amount Discounted","Percentage Discounted"]
-        metrics = ["Frequent Phrases","Aggregate Statistics"]
+        metrics = ["Revenue","Prices","Rating","Reviews Count","Amount Discounted","Percent Discounted","Frequent Phrases"]
+
     select_metric = st.selectbox('Select a metric for visualization', metrics)
-    
+    if "Best" in select_metric: 
+        all_df = df[df.Best_Seller == True]
+    elif "Prime" in select_metric: 
+        all_df = df[df.Prime == True]
+    elif "Past" in select_metric: 
+        all_df = df[df['Sales_Volume_Past_Month'].notnull()]
+    elif 'Discounted' in select_metric: 
+        all_df = df[df['Discount'].notnull()]
+    else: 
+        all_df = df
 #######################
 
-def bar_mean(title): 
+def bar_metric(title,metric="Mean"): 
     if selected_category == "All": 
+        ptype,title = title.split(" - ")
         data = stats[stats.Title == title]
-        data = data.sort_values(by=['Mean','Type','Keyword'], ascending=[False,False,True])
+        data = data[data["Product Type"] == ptype]
+        data = data.sort_values(by=[metric,'Search Type','Keyword'], ascending=[False,False,True])
         chart = alt.Chart(data).mark_bar(opacity=0.7).encode(
             x=alt.X('Keyword:O', axis=alt.Axis(labelAngle=90), title='Keyword'),  
-            y=alt.Y('Mean:Q', title=f"Average {title}").stack(None),
-            color=alt.Color('Type:N', legend=alt.Legend(title="Type of Search Result",orient="bottom"))  
+            y=alt.Y(f'{metric}:Q', title=f"{metric} {title}").stack(None),
+            color=alt.Color('Search Type:N', legend=alt.Legend(title="Type of Search Result",orient="bottom"))  
         )
     else: 
-        relevant = stats[stats.Keyword==selected_category.lower()]
-        # if title == 'Minimum Revenue': 
-
+        data = stats[stats.Title == title]
+        data = data[data.Keyword == selected_category.lower()]
+        data = data.sort_values(by=[metric,'Search Type','Product Type'], ascending=[False,False,True])
+        chart = alt.Chart(data).mark_bar(opacity=0.7).encode(
+            x=alt.X(f'{metric}:Q', axis=alt.Axis(labelAngle=0), title=f"{metric} {title}").stack(None),  
+            y=alt.Y('Product Type:O',title='Product Type'),
+            color=alt.Color('Search Type:N', legend=alt.Legend(title="Type of Search Result",orient="bottom"))  
+        )
     st.altair_chart(chart, use_container_width=True)
 
-def bar_median(title): 
-    data = stats[stats.Title == title]
-    data = data.sort_values(by=['Median','Type','Keyword'], ascending=[False,False,True])
-    chart = alt.Chart(data).mark_bar(opacity=0.7).encode(
-        x=alt.X('Keyword:O', axis=alt.Axis(labelAngle=90), title='Keyword'),  
-        y=alt.Y('Median:Q', title=f"Median {title}").stack(None),
-        color=alt.Color('Type:N', legend=alt.Legend(title="Type of Search Result",orient="bottom"))  
-    )
-    st.altair_chart(chart, use_container_width=True)
 
 def bar_searchterms(topK): 
-    data = df
+    data = all_df
     terms = []
     for term in data.Search_Term.head(topK): 
         terms.extend(term.split("; "))
@@ -118,9 +127,16 @@ def bar_searchterms(topK):
     fig = px.bar(chart, x='Frequency', y='Search Term', orientation='h')
     st.plotly_chart(fig)
 
+original_cmap = plt.cm.Blues
+n_colors = 256
+colors = original_cmap(np.linspace(0, 1, n_colors))
+darkening_factor = 0.6
+colors[-n_colors//8:] *= darkening_factor
+colors = np.clip(colors, 0, 1)
+darkened_cmap = LinearSegmentedColormap.from_list('darkened_blues', colors)
 
 def wordcloud(n):
-    data = df.Title.head(n)
+    data = all_df.Title.head(n)
     # all_ngrams = []
     # for string in data:
     #     tokens = nltk.word_tokenize(string)
@@ -130,7 +146,7 @@ def wordcloud(n):
     #     all_ngrams.extend(ngrams_list)
     # data = " ".join(all_ngrams)
     data = " ".join(data)
-    wordcloud = WordCloud(width=1600, height=800, background_color='white',colormap='winter').generate(data)
+    wordcloud = WordCloud(width=2000, height=1000, background_color='white',colormap=darkened_cmap).generate(data)
     fig, ax = plt.subplots()
     ax.imshow(wordcloud, interpolation='bilinear')
     ax.axis('off')
@@ -141,69 +157,30 @@ def wordcloud(n):
     # st.dataframe(word_frequencies)
     
 def histogram(title): 
-    data = df[title]
+    data = all_df[title]
     fig = px.histogram(data, nbins=30, labels={'value': 'Value', 'count': 'Frequency'})
     st.plotly_chart(fig)
      
 #######################
 col = st.columns((4, 5), gap='large')
-with col[0]: 
-
-    if select_metric == "Frequent Phrases": 
-        if 1000 < len(df): val = 1000
-        else: val = len(df)
-        topK = st.number_input(
-            'Enter a value for K:',
-            min_value=1, max_value=len(df), value=val, step=100)
-        st.markdown('#### Frequent Phrases in Titles for the Top K Products')
-        wordcloud(topK)
-        st.markdown('#### Frequent Search Terms for the Top K Products')
-        bar_searchterms(topK)
-    else: 
-        st.markdown(f'#### {select_metric}')
-        if selected_category == "All": 
-            st.markdown('Average')
-            bar_mean(select_metric)
-            st.markdown('Median')
-            bar_median(select_metric)
-        else: 
-            # if select_metric in ["Minimum_Revenue","Price","Rating","Reviews_Count"]:
-            #     histogram(select_metric)
-            # elif 'Strikethrough' in select_metric: 
-                # relevant = stats[stats.Keyword==selected_category]
-                # discounts = relevant[relevant.Title == "Amount Discounted"]
-                # percents = relevant[relevant.Title == "Percentage Discounted"]
-                # toprint = discounts
-                # toprint = discounts.update(percents)
-                # st.write(toprint)
-            relevant = stats[stats.Keyword==selected_category.lower()]
-            st.dataframe(relevant,
-                column_order=("Title", "Type", "Mean","Median","Mode","Mode Freq","Max","Min","Percentile25","Percentile75"),
-                hide_index=True
-            )
 
 with col[1]:
     if "Best" in select_metric: 
         st.markdown('#### Top Best Sellers by Minimum Revenue')
-        all_df = df[df.Best_Seller == True]
     elif "Prime" in select_metric: 
         st.markdown('#### Top Prime Products by Minimum Revenue')
-        all_df = df[df.Prime == True]
     elif "Past" in select_metric: 
         st.markdown('#### Top Products with Past Month Sales Info by Minimum Revenue')
-        all_df = df[df['Sales_Volume_Past_Month'].notnull()]
-    elif "Discount" in select_metric or 'Strikethrough' in select_metric: 
+    elif 'Discounted' in select_metric: 
         st.markdown('#### Top Products with Price Strikethrough by Minimum Revenue')
-        all_df = df[df['Discount'].notnull()]
     else: 
         st.markdown('#### Top Products by Minimum Revenue')
-        all_df = df
-
-    if 1000 < len(df): val = 1000
-    else: val = len(df)
+        
+    if len(all_df) > 1000: val = 1000
+    else: val = len(all_df)
     topK = st.number_input(
             'Enter a value for K:',
-            min_value=1, max_value=len(df), value=val, step=100)
+            min_value=1, max_value=len(all_df), value=val, step=100,key="table")
     top_df = all_df.head(topK)
     st.markdown(f'Top {len(top_df)} out of {len(all_df)} Total Products')
     st.dataframe(top_df,
@@ -229,6 +206,36 @@ with col[1]:
             - :blue[**Revenue**]: Price multiplied by the count of reviews. 
             - :blue[**Past Month Sales**]: These are products which have approximate sales information for the past month, e.g., "2K+ bought in past month."
             ''')
-        
     
-    
+
+with col[0]: 
+    if "Frequent Phrases" in select_metric: 
+        # if 1000 < len(all_df): val = 1000
+        # else: val = len(all_df)
+        # topK = st.number_input(
+        #     'Enter a value for K:',
+        #     min_value=1, max_value=len(all_df), value=val, step=100,key="phrases")
+        st.markdown(f'#### Frequent Phrases in Titles for the Top {topK} Products')
+        wordcloud(topK)
+        st.markdown(f'#### Frequent Search Terms for the Top {topK} Products')
+        bar_searchterms(topK)
+    else: 
+        st.markdown(f'#### {select_metric}')
+        st.markdown('Average')
+        bar_metric(select_metric)
+        st.markdown('Median')
+        bar_metric(select_metric,"Median")
+        # if select_metric in ["Minimum_Revenue","Price","Rating","Reviews_Count"]:
+        #     histogram(select_metric)
+        # elif 'Strikethrough' in select_metric: 
+            # relevant = stats[stats.Keyword==selected_category]
+            # discounts = relevant[relevant.Title == "Amount Discounted"]
+            # percents = relevant[relevant.Title == "Percentage Discounted"]
+            # toprint = discounts
+            # toprint = discounts.update(percents)
+            # st.write(toprint)
+        # relevant = stats[stats.Keyword==selected_category.lower()]
+        # st.dataframe(relevant,
+        #     column_order=("Title", "Type", "Mean","Median","Mode","Mode Freq","Max","Min","Percentile25","Percentile75"),
+        #     hide_index=True
+        # )
