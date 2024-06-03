14da3d926ce00ed397c11266df6beae842eed4e9
Amy Weng <90800337+amycweng@users.noreply.github.com>
2024-06-04 02:15:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,9 @@ from collections import Counter
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
-
+import nltk
+from nltk.util import ngrams
+import string 
 #######################
 st.set_page_config(
     page_title="Amazon Jewelry Dashboard",
@@ -50,10 +52,12 @@ products = products.drop(columns=['Unnamed: 0'])
 stats = pd.read_csv('data/stats.csv')
 stats['Product Type'] = stats['Product Type'].astype(str)
 
+
 allterms = {}
 for keyword in products.Keyword:
     if keyword not in allterms: 
         allterms[keyword] = 0
+
 
 
 with st.sidebar:
@@ -87,6 +91,19 @@ with st.sidebar:
         all_df = df[df['Discount'].notnull()]
     else: 
         all_df = df
+
+all_df['URL'] = 'https://amazon.com' + df['URL'].astype(str)
+all_prod = all_df.to_dict(orient='records')
+unique_asin = {info['ASIN']: 0 for info in all_prod}
+unique = {}
+for info in all_prod: 
+    if info["ASIN"] not in unique: 
+        unique[info["ASIN"]] = info
+    else: 
+        unique[info["ASIN"]]["Keyword"] = unique[info["ASIN"]]["Keyword"] + "; " + info["Keyword"]
+        unique[info["ASIN"]]["Search_Term"] = unique[info["ASIN"]]["Search_Term"] + "; " + info["Search_Term"]
+        unique[info["ASIN"]]["Position"] = unique[info["ASIN"]]["Position"] + "; " + info["Position"]
+unique = pd.DataFrame(unique.values())
 #######################
 
 def bar_metric(title,metric="Mean"): 
@@ -113,51 +130,51 @@ def bar_metric(title,metric="Mean"):
 
 
 def bar_searchterms(topK): 
-    data = all_df
+    data = unique
     terms = []
     for term in data.Search_Term.head(topK): 
         terms.extend(term.split("; "))
-    terms = Counter(terms).most_common(n=50)
+    terms = Counter(terms)
+    topterms = Counter(terms).most_common(n=25)
     chart = pd.DataFrame({
-        'Search Term': [x[0] for x in terms],
-        'Frequency': [x[1] for x in terms]
+        'Search Phrase': [x[0] for x in topterms],
+        'Frequency': [x[1] for x in topterms]
     }
     )
     chart = chart.sort_values(by='Frequency',ascending=True)
-    fig = px.bar(chart, x='Frequency', y='Search Term', orientation='h')
+    fig = px.bar(chart, x='Frequency', y='Search Phrase', orientation='h')
     st.plotly_chart(fig)
+    return terms
 
 original_cmap = plt.cm.Blues
 n_colors = 256
 colors = original_cmap(np.linspace(0, 1, n_colors))
 darkening_factor = 0.6
-colors[-n_colors//8:] *= darkening_factor
+colors[-n_colors//5:] *= darkening_factor
 colors = np.clip(colors, 0, 1)
 darkened_cmap = LinearSegmentedColormap.from_list('darkened_blues', colors)
 
 def wordcloud(n):
-    data = all_df.Title.head(n)
-    # all_ngrams = []
-    # for string in data:
-    #     tokens = nltk.word_tokenize(string)
-    #     for i in range(n): 
-    #         ngrams_list = list(ngrams(tokens, i))
-    #         ngrams_list = ['_'.join(gram) for gram in ngrams_list]
-    #     all_ngrams.extend(ngrams_list)
-    # data = " ".join(all_ngrams)
-    data = " ".join(data)
-    wordcloud = WordCloud(width=2000, height=1000, background_color='white',colormap=darkened_cmap).generate(data)
+    data = unique.Title.head(n)
+    all_ngrams = []
+    for item in data:
+        translator = str.maketrans('', '', string.punctuation)
+        item = item.translate(translator)
+        tokens = nltk.word_tokenize(item)
+        for i in range(2,4): 
+            ngrams_list = list(ngrams(tokens, i))
+            ngrams_list = ['_'.join(gram) for gram in ngrams_list]
+        all_ngrams.extend(ngrams_list)
+    data = Counter(all_ngrams)
+    wordcloud = WordCloud(width=2000, height=1000, background_color='white',colormap=darkened_cmap).generate_from_frequencies(data)
     fig, ax = plt.subplots()
     ax.imshow(wordcloud, interpolation='bilinear')
     ax.axis('off')
     st.pyplot(fig)
-    # word_frequencies = pd.DataFrame(list(wordcloud.words_.items()), columns=['Word', 'Frequency'])
-    # word_frequencies = word_frequencies.sort_values(by=['Frequency','Word'], ascending=[False,True])
-    # st.write("Word Frequencies:")
-    # st.dataframe(word_frequencies)
+    return data
     
 def histogram(title): 
-    data = all_df[title]
+    data = unique[title]
     fig = px.histogram(data, nbins=30, labels={'value': 'Value', 'count': 'Frequency'})
     st.plotly_chart(fig)
      
@@ -165,6 +182,7 @@ def histogram(title):
 col = st.columns((4, 5), gap='large')
 
 with col[1]:
+
     if "Best" in select_metric: 
         st.markdown('#### Top Best Sellers by Minimum Revenue')
     elif "Prime" in select_metric: 
@@ -176,15 +194,15 @@ with col[1]:
     else: 
         st.markdown('#### Top Products by Minimum Revenue')
         
-    if len(all_df) > 1000: val = 1000
-    else: val = len(all_df)
+    if len(unique) > 1000: val = 1000
+    else: val = len(unique)
     topK = st.number_input(
             'Enter a value for K:',
-            min_value=1, max_value=len(all_df), value=val, step=100,key="table")
-    top_df = all_df.head(topK)
-    st.markdown(f'Top {len(top_df)} out of {len(all_df)} Total Products')
+            min_value=1, max_value=len(unique), value=val, step=100,key="table")
+    top_df = unique.head(topK)
+    st.markdown(f'Top {len(top_df)} out of {len(unique)} Total Products')
     st.dataframe(top_df,
-                column_order=("Title", "Minimum_Revenue", "Price","Currency","Sponsored","Rating","Reviews_Count","Best_Seller","Sales_Volume_Past_Month","Prime","Price_Strikethrough","Discount","Discount_Rate","Search_Term", "Position","ASIN","URL","Image_URL"),
+                column_order=("Title", "Minimum_Revenue", "Price","Currency","Sponsored","Rating","Reviews_Count","Best_Seller","Sales_Volume_Past_Month","Prime","Price_Strikethrough","Discount","Discount_Rate","Keyword","Search_Term", "Position","ASIN","URL","Image_URL"),
                 hide_index=True,
                 width=None,
                 column_config={
@@ -194,20 +212,25 @@ with col[1]:
                         min_value=min(df.Minimum_Revenue),
                         max_value=max(df.Minimum_Revenue),
                     ),
-                    "Sponsored": st.column_config.TextColumn("Paid Search Result",)
+                    "Sponsored": st.column_config.TextColumn("Paid Search Result",),
+                    "Search_Term": st.column_config.TextColumn("Search Phrases",),
+                    "Search_Term": st.column_config.TextColumn("Search Keywords",),
+                    "URL": st.column_config.LinkColumn("View Detailed Product Page"),
+                    "Image_URL": st.column_config.LinkColumn("View Product Image")
                     }
                     
                 )
     
     with st.expander('About', expanded=True):
         st.markdown('''
-            - Data: [**Amazon**](amazon.com) using [Oxylab's E-Commerce Scraper API](https://oxylabs.io/products/scraper-api/ecommerce/amazon). Of the scraped results, only products with at least one review are included for analysis.
-            - :blue[**Search Term**] and :blue[**Position**]: The relevant search terms and the product's positions in the search results for those terms (across all pages) are divided respectively by semicolons. These terms are among those with the highest search volume associated with more general key terms based on [Keyword Tool](https://keywordtool.io/).
+            - Data: [**Amazon**](amazon.com) using [Oxylab's E-Commerce Scraper API](https://oxylabs.io/products/scraper-api/ecommerce/amazon). Of the scraped results, only products with at least one review are included for analysis. 
+            - :blue[**Search Phrases**] and :blue[**Position**]: The relevant search phrases and the product's positions in the search results for those phrases (across all pages) are divided respectively by semicolons. These phrases are among those with the highest search volume associated with more general key terms (**Search Keywords**) based on [Keyword Tool](https://keywordtool.io/).
             - :blue[**Revenue**]: Price multiplied by the count of reviews. 
             - :blue[**Past Month Sales**]: These are products which have approximate sales information for the past month, e.g., "2K+ bought in past month."
             - :blue[**Organic**]: These are the non-sponsored search results returned for a query. 
             - :blue[**Sponsored/Paid**]: These are the paid advertisements among the results that are returned for a query. 
             ''')
+    
 
 with col[0]: 
     if "Frequent Phrases" in select_metric: 
@@ -217,9 +240,20 @@ with col[0]:
         #     'Enter a value for K:',
         #     min_value=1, max_value=len(all_df), value=val, step=100,key="phrases")
         st.markdown(f'#### Frequent Phrases in Titles for the Top {topK} Products')
-        wordcloud(topK)
+        frequencies = wordcloud(topK)
+        st.write(f"Top {len(frequencies)} phrases")
+        frequencies = pd.DataFrame.from_dict(frequencies, orient='index').reset_index()
+        frequencies.columns = ['Phrase', 'Frequency']
+        frequencies = frequencies.sort_values(by=['Frequency','Phrase'], ascending=[False,True])
+        frequencies.reset_index(drop=True, inplace=True)
+        st.dataframe(frequencies,width=None)
         st.markdown(f'#### Frequent Search Terms for the Top {topK} Products')
-        bar_searchterms(topK)
+        terms = bar_searchterms(topK)
+        terms = pd.DataFrame.from_dict(terms, orient='index').reset_index()
+        terms.columns = ['Search Phrase', 'Frequency']
+        terms = terms.sort_values(by=['Frequency','Search Phrase'], ascending=[False,True])
+        terms.reset_index(drop=True, inplace=True)
+        st.dataframe(terms,width=None)
     else: 
         st.markdown(f'#### {select_metric}')
         st.markdown('Average')
